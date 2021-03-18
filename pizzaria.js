8e95ddbc76f3102536034806a518164241fb37c6
Pedro <pedro.fortunato07@gmail.com>
2021-03-18 13:43:04 -0300

    o:

--- pizzaria.js
+++ pizzaria.js
@@ -1,4 +1,12 @@
-
 function apertarBotaoAlerta() {
-    document.getElementById("alerta").style.display = "none";
+    document.getElementById('alerta').style.display = 'none';
 }
+
+botao = document.querySelector('#comecar');
+botao.addEventListener('click', function() {
+    let novoTitulo = document.querySelector('#nome-pizzaria').value;
+    window.location.href = 'jogo.html';
+    for (let i = 0; i < 40; i++) 
+        document.title = novoTitulo;
+});
+
