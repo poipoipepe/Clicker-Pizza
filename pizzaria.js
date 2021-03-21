8d6e95a7b7fcc46ba6b57a3e60c044276a00ff6a
Pedro <pedro.fortunato07@gmail.com>
2021-03-21 19:45:39 -0300

    cozinheiro

--- pizzaria.js
+++ pizzaria.js
@@ -1,11 +1,49 @@
-let quantidadePizza = 0;
+// quantidade inicial de pizzas
+let Pizzas = 0;
+
+// cozinheiro
+let cozinheiro = 0;
+let quantCozinheiro = 0;
+let precoCozinheiro = 10;
+
+let compraCozinheiro = document.querySelector('#cozinheiro');
+compraCozinheiro.addEventListener('click', function(){
+    if (Pizzas >= precoCozinheiro){
+        quantCozinheiro++;
+        cozinheiro = cozinheiro + 0.2;
+        Pizzas = Pizzas - precoCozinheiro;
+        precoCozinheiro = precoCozinheiro + precoCozinheiro/25;
+        compraCozinheiro.innerHTML = 'Cozinheiro: 0.2 Pizzas/s <strong>'+ quantCozinheiro +'</strong><br> Preço: ' + precoCozinheiro.toFixed(2) + ' Pizzas';
+        Pizzas = Pizzas + cozinheiro;
+        if (Pizzas >= 1000)
+            atualizaPizzas.innerHTML = 'Pizzas: ' + Pizzas.toFixed(0);
+        else
+            atualizaPizzas.innerHTML = 'Pizzas: ' + Pizzas.toFixed(2);
+    }
+});
+
+// atualiza a quantidade de pizzas
 atualizaPizzas = document.querySelector('#numeroPizzas');
 
+function update(){
+    Pizzas = Pizzas + cozinheiro;
+    if (Pizzas >= 1000)
+        atualizaPizzas.innerHTML = 'Pizzas: ' + Pizzas.toFixed(0);
+    else
+        atualizaPizzas.innerHTML = 'Pizzas: ' + Pizzas.toFixed(2);
+    
+    console.log(cozinheiro);
+}
+setInterval(update, 1000);
+
+// clique na pizza
 let clique = document.querySelector('#clique');
+clique.addEventListener('click', cliquePizza);
+function cliquePizza(){
+    Pizzas++;
+    atualizaPizzas.innerHTML = 'Pizzas: ' + Pizzas.toFixed(2);
 
-clique.addEventListener('click', function(){
-    quantidadePizza++;
-    atualizaPizzas.innerHTML = 'Pizzas: ' + quantidadePizza;
+    console.log(Pizzas);
+}
 
-    console.log(quantidadePizza);
-});
+
